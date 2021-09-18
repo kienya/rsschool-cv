0c363bdc21e38cd10187e5df10b98f7925beb6dd
Kiyenya Kanstantsin <kienya.ru@gmail.com>
2021-09-18 19:30:13 +0300

    feat: add languages

--- js/script.js
+++ js/script.js
@@ -28,3 +28,51 @@ links.forEach(item => item.addEventListener('click', (e) => {
     });
 }));
 
+
+//Languages
+const languages = {
+    "russian": 100,
+    "belarusian": 77,
+    "english": 13,
+}
+
+const languagesList = document.querySelector('#languages-list');
+
+let calcLevel = (result, idx) => {
+    const levels = languagesList.querySelectorAll('.language__progress')[idx].querySelectorAll('.language__level');
+    result = Math.ceil(result / 10);
+    levels.forEach((elem, i) => {
+        if (result === 10) {
+            elem.classList.add('language__level_success')
+        } else if (result > i + 1) {
+            elem.classList.add('language__level_success')
+        } else if (result === i + 1) {
+            elem.classList.add('language__level_active')
+        }
+    });
+}
+
+let addLanguages = () => {
+    let counter = 0;
+    for (let language in languages) {
+        languagesList.innerHTML +=
+            `<li class="list__item language">
+                                        <div class="language__caption">${language[0].toUpperCase() + language.slice(1)}</div>
+                                        <div class="language__progress">                                        
+                                            <span class="language__level"></span>
+                                            <span class="language__level"></span>
+                                            <span class="language__level"></span>
+                                            <span class="language__level"></span>
+                                            <span class="language__level"></span>
+                                            <span class="language__level"></span>
+                                            <span class="language__level"></span>
+                                            <span class="language__level"></span>
+                                            <span class="language__level"></span>
+                                            <span class="language__level"></span>
+                                        </div>
+                                    </li>`;
+        calcLevel(languages[language], counter++);
+    }
+}
+addLanguages();
+
